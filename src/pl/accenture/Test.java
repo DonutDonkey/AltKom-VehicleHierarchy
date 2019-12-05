b81184338a3bd73b3e38ed8804d15105a8bc5e3b
DonutDonkey <34782950+DonutDonkey@users.noreply.github.com>
2019-12-05 13:32:59 +0100

    Initial Commit

---
+++ src/pl/accenture/Test.java
@@ -0,0 +1,23 @@
+package pl.accenture;
+
+public class Test {
+
+    public static void main(String[] args) {
+        // write your code here
+        Vehicle [] vehicles = {new Car("Car"), new Ship("Ship"), new Plane("Plane"),
+                               new Truck("Truck"), new SuperCar("SuperCar")};
+
+        for (Vehicle ve : vehicles) {
+            ve.go();
+            ve.stop();
+            System.out.println("FUEL CONSUMPION : " + ve.calculateFuelConsumption());
+
+            if(ve instanceof Flying) {
+                ((Flying) ve).callAirControl();
+            }
+
+            System.out.println("##########");
+        }
+    }
+}
+
